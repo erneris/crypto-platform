71b1ce46252ce83cd69588a7ce788e784a653981
New User <newuser@example.com>
2025-03-11 20:40:46 +0200

    deploy 13

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,7 +1,7 @@
 import { URL, fileURLToPath } from 'node:url';
 import path from 'node:path';
-// @ts-ignore
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
